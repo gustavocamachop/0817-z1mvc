2e8cc4a488f3d6485b3e555923be0723eec67b92
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 10:08:01 -0400

    solucion completa MVC

---
+++ MVCproduct/MVCproduct/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace MVCproduct.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
